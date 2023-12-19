8988744ecad32fec9725ced6ebbef0686a82b75a
anki356 <39798400+anki356@users.noreply.github.com>
2023-12-19 16:42:38 +0530

    Save

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ app.use("/api/",TeacherDashboardRouter)
 app.use(errorHandlerMiddleware);
 app.use(notFound);
  const start = async () => {
+  console.log(process.env.DB_URL)
     try {
      await mongoose.connect(process.env.DB_URL, {
         useUnifiedTopology: true,

540271cce8283c2b689edd3039f1202d9ce79197
anki356 <39798400+anki356@users.noreply.github.com>
2023-12-19 18:46:30 +0530

    save

--- util/EmailFormats/homeworkEmail.js
+++ util/EmailFormats/homeworkEmail.js
@@ -2,7 +2,7 @@ import {  marked } from "marked"
 const homeworkEmail=(student_name,title,subject_name,grade_name,teacher_name)=>{
 
 
-let content=`a
+let content=`
 **Dear ${student_name}**
 
 - Homework: ${title} is given in Class Name:${subject_name} of ${grade_name } By ${teacher_name} is pending.Kindly Upload the Solution.

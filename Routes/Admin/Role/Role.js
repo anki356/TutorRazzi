ce7574e1a7a964b3e534099b15ae5b55685cc3bc
anki356 <39798400+anki356@users.noreply.github.com>
2023-12-26 13:23:40 +0530

    Save

--- Routes/Admin/Role/Role.js
+++ Routes/Admin/Role/Role.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { authVerify } from '../../../controllers/Admin/Auth/Auth.js'
-import { addRole, deleteUser, getAllRoles, getRoleDetails } from '../../../controllers/Admin/Role/Role.js'
+import {  deleteUser, getAllRoles, getRoleDetails } from '../../../controllers/Admin/Role/Role.js'
 const router=express.Router()
 router.get("/roles",authVerify,getAllRoles)
 router.get("/role-details",authVerify,getRoleDetails)

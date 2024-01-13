6874e5c0d345836008cf3e545496e878a92aad53
anki356 <39798400+anki356@users.noreply.github.com>
2024-01-13 12:03:07 +0530

    Save

--- errorHandler/notFound.js
+++ errorHandler/notFound.js
@@ -1,7 +1,7 @@
 import { responseObj } from "../util/response.js";
 
 const notFound = (req,res) => {
-      const response = responseObj(false,  null,"Requested Route not found",error);
+      const response = responseObj(false,  null,"Requested Route not found",[]);
       return res.status(404).json(response);
 }
 export default notFound;

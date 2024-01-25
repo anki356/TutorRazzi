5660bb8f560db2e183fabaa6705767a09ea72834
anki356 <39798400+anki356@users.noreply.github.com>
2024-01-25 13:40:34 +0530

    Save

--- util/EmailFormats/newResourceRequested.js
+++ util/EmailFormats/newResourceRequested.js
@@ -6,7 +6,7 @@ let content=`# Request Resource Acknowledgement
             
 Dear ${user_name},
             
-You have successfully requested for a resource from the admin of this platform. You will be notified once your request is approved by an Admin.
+You have successfully requested for a resource from the admin of this platform.
 Your Request Id is ${resourcerequestcount+1}
 
 Best Regards,  

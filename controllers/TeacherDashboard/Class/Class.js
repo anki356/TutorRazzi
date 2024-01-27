b68eae54a15b63dcc24153eed0530a08a53c728f
anki356 <39798400+anki356@users.noreply.github.com>
2024-01-27 16:01:13 +0530

    Save

--- controllers/TeacherDashboard/Class/Class.js
+++ controllers/TeacherDashboard/Class/Class.js
@@ -441,7 +441,7 @@ const setReminder = async (req, res, next) => {
   }
   const getClassDetails=async(req,res)=>{
     let classDetails = {}
-  classDetails = await Class.findOne({ _id: req.query.class_id }, { start_time: 1, end_time: 1, details: 1, grade: 1, subject: 1, teacher_id: 1, notes: 1,  materials: 1, recordings: 1 }).populate({
+  classDetails = await Class.findOne({ _id: req.query.class_id }).populate({
     path: 'teacher_id', select: {
      name: 1,profile_image:1
     }

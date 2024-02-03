c89f758ac178ddb5ad06e30db8663aab219dfe02
anki356 <39798400+anki356@users.noreply.github.com>
2024-02-03 18:14:43 +0530

    SAve

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.use("/api/",WebsiteRouter)
 app.use(errorHandlerMiddleware);
 
 app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
+app.set('views', "./util" + '/views');
 app.get('/reset-password/:token', (req, res) => {
   const token = req.params.token;
   return res.render('reset-password', { token, errorMessage: null });

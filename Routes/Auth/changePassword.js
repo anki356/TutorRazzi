5ba4a76cdc974a227cbd70700a77bdde8a3954a9
anki356 <39798400+anki356@users.noreply.github.com>
2024-02-03 18:51:31 +0530

    Save

--- Routes/Auth/changePassword.js
+++ Routes/Auth/changePassword.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import User from '../../models/User';
+import User from '../../models/User.js';
 import bcrypt from 'bcrypt'
 const router=express.Router()
 router.post("/auth/Change-password/:token",async(req,res)=>{

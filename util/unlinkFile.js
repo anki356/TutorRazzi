1a6bc1154266c6c4679931a9ef9c677ac1cd66d8
anki356 <39798400+anki356@users.noreply.github.com>
2024-02-03 10:29:21 +0530

    Save

--- util/unlinkFile.js
+++ util/unlinkFile.js
@@ -1,7 +1,8 @@
 import fs from 'fs'
 import path from 'path'
 const unlinkFile=async(fileName)=>{
-    
+    if(fs.existsSync(path.resolve()+"/uploads/"+""+fileName)){
     fs.unlinkSync(path.resolve()+"/uploads/"+""+fileName)
+    }
 }
 export default unlinkFile

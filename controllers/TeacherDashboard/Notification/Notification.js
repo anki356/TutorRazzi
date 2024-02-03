6b3a86a3f51ea7edf87225346f0b93374ab45c67
anki356 <39798400+anki356@users.noreply.github.com>
2024-02-03 13:53:32 +0530

    Save

--- controllers/TeacherDashboard/Notification/Notification.js
+++ controllers/TeacherDashboard/Notification/Notification.js
@@ -5,8 +5,8 @@ const getNotifications=async(req,res)=>{
     const notifications=await Notification.find({user_id:req.user._id})
     return res.json(responseObj(true,notifications,"Notifications"))
 }
-const clearNotification=(req,res)=>{
-    Notification.deleteMany({user_id:req.user._id})
+const clearNotification=async (req,res)=>{
+  await  Notification.deleteMany({user_id:req.user._id})
     return res.json(responseObj(true,[],"Notifications Cleared"))
 }
 export {getNotifications,clearNotification}

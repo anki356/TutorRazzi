e5ac52650efc5f9fe8d88c12fe2917d3248148ec
anki356 <39798400+anki356@users.noreply.github.com>
2024-02-03 18:32:24 +0530

    SAve

---
+++ Routes/Auth/changePassword.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import User from '../../models/User';
+import bcrypt from 'bcrypt'
+const router=express.Router()
+router.post("/auth/Change-password/:token",async(req,res)=>{
+    const { token } = req.params;
+    const { password } = req.body;
+
+    const tokenUser = await User.findOne({ resetToken: token });
+
+    if (!tokenUser) {
+        return res.render('reset-password', { token, errorMessage: 'Invalid or expired reset token.' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await tokenUser.updateOne({
+        password: hashedPassword,
+        token: null
+    });
+
+    // const response = responseJson(true, null, 'Password has changed successfuly', StatusCodes.OK, []);
+    return res.render('reset-password-confirmed', { message: 'Password has changed successfuly.' });
+
+    
+})

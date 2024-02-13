cdf2ded9cb9c31720a38e9e5311b25a604d7aca6
Ankita <anki356@gmil.com>
2024-02-13 15:19:23 +0530

    Save

--- controllers/AcademicManager/Class/Class.js
+++ controllers/AcademicManager/Class/Class.js
@@ -685,7 +685,9 @@ const getUpcomingClasses=async(req,res,next)=>{
   }
   const getClassDetails = async (req, res, next) => {
     let classDetails = {}
-    classDetails = await Class.findOne({ _id: req.query.class_id }, { start_time: 1, end_time: 1, details: 1, grade: 1, subject: 1, teacher_id: 1, notes: 1,  materials: 1, recordings: 1,response:1,reason_disliking:1,curriculum:1 }).populate({
+    classDetails = await Class.findOne({ _id: req.query.class_id,end_time:{
+      $lte:moment().format("YYYY-MM-DDTHH:mm:ss")
+    } }, { start_time: 1, end_time: 1, details: 1, grade: 1, subject: 1, teacher_id: 1, notes: 1,  materials: 1, recordings: 1,response:1,reason_disliking:1,curriculum:1 }).populate({
       path: 'teacher_id', select: {
        name: 1,profile_image:1
       }
@@ -694,6 +696,9 @@ const getUpcomingClasses=async(req,res,next)=>{
         name: 1,mobile_number:1,profile_image:1
       }
     })
+    if(classDetails===null){
+      return res.json(responseObj(false,null,"Invalid Class Id"))
+    }
     let studentDetails=await Student.findOne({user_id:classDetails.student_id},{
       grade:1,
       curriculum:1,
@@ -726,7 +731,7 @@ const getUpcomingClasses=async(req,res,next)=>{
   }
   const getTrialClassDetails = async (req, res, next) => {
     let classDetails = {}
-    classDetails = await Class.findOne({ _id: req.query.class_id }, { start_time: 1, end_time: 1, details: 1, grade: 1, subject_id: 1, teacher_id: 1, notes: 1,  materials: 1, recordings: 1,response:1,reason_disliking:1,curriculum:1 }).populate({
+    classDetails = await Class.findOne({ _id: req.query.class_id,class_type:"Trial" }, { start_time: 1, end_time: 1, details: 1, grade: 1, subject_id: 1, teacher_id: 1, notes: 1,  materials: 1, recordings: 1,response:1,reason_disliking:1,curriculum:1 }).populate({
       path: 'teacher_id', select: {
        name: 1,profile_image:1
       }
@@ -735,6 +740,9 @@ const getUpcomingClasses=async(req,res,next)=>{
         name: 1,mobile_number:1,profile_image:1
       }
     })
+    if(classDetails===null){
+      return res.json(responseObj(false,null,"Invalid Class Id"))
+    }
     let studentDetails=await Student.findOne({user_id:classDetails.student_id},{
       grade:1,
       curriculum:1,
@@ -849,7 +857,9 @@ const getExtraClassRequests=async(req,res)=>{
 
 const getUpcomingClassDetails=async(req,res)=>{
   let classDetails = {}
-  classDetails = await Class.findOne({ _id: req.query.class_id }, { start_time: 1, end_time: 1, details: 1, grade: 1, subject: 1, teacher_id: 1, notes: 1,materials:1 }).populate({
+  classDetails = await Class.findOne({ _id: req.query.class_id ,start_time:{
+    $gte:moment().format("YYYY-MM-DDTHH:mm:ss")
+  }}, { start_time: 1, end_time: 1, details: 1, grade: 1, subject: 1, teacher_id: 1, notes: 1,materials:1 }).populate({
     path: 'teacher_id', select: {
      name: 1,profile_image:1
     }
@@ -858,6 +868,9 @@ const getUpcomingClassDetails=async(req,res)=>{
       name: 1,mobile_number:1,profile_image:1
     }
   })
+  if(classDetails===null){
+    return res.json(responseObj(false,null,"Invalid Class Id"))
+  }
   let studentDetails=await Student.findOne({user_id:classDetails.student_id},{
     grade:1,
     curriculum:1,

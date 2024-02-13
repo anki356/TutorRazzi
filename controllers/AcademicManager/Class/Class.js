796485ba0eae6034b8ff0025e85bade51cdc3c41
Ankita <anki356@gmil.com>
2024-02-13 13:58:54 +0530

    Save

--- controllers/AcademicManager/Class/Class.js
+++ controllers/AcademicManager/Class/Class.js
@@ -730,7 +730,7 @@ const getUpcomingClasses=async(req,res,next)=>{
   }
   const getTrialClassDetails = async (req, res, next) => {
     let classDetails = {}
-    classDetails = await Class.findOne({ _id: req.query.class_id }, { start_time: 1, end_time: 1, details: 1, grade: 1, subject_id: 1, teacher_id: 1, notes: 1,  materials: 1, recordings: 1,response:1,reason_disliking:1 }).populate({
+    classDetails = await Class.findOne({ _id: req.query.class_id }, { start_time: 1, end_time: 1, details: 1, grade: 1, subject_id: 1, teacher_id: 1, notes: 1,  materials: 1, recordings: 1,response:1,reason_disliking:1,curriculum:1 }).populate({
       path: 'teacher_id', select: {
        name: 1,profile_image:1
       }

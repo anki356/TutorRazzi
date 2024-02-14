ab60306ac5d05f984e07f45709563adb2512b4a2
Ankita <anki356@gmil.com>
2024-02-14 11:38:47 +0530

    Save

--- controllers/AcademicManager/Class/Class.js
+++ controllers/AcademicManager/Class/Class.js
@@ -723,11 +723,18 @@ const getUpcomingClasses=async(req,res,next)=>{
   })
     
     let reminderResponse = await Reminder.findOne({ class_id:req.query.class_id })
-    let ratingsResponse=await Review.findOne({
-      class_id:req.query.class_id
+    let classRatingsResponse=await Review.findOne({
+      class_id:req.query.class_id,
+      given_by:classDetails.student_id,
+      teacher_id:null
     })
-   
-    res.json(responseObj(true, { classDetails: classDetails, reminderResponse: reminderResponse,studentDetails:studentDetails,homeworkResponse:homeworkResponse,taskResponse:taskResponse,teacherDetails:teacherDetails,ratingsResponse:ratingsResponse }, null))
+   let teacherRatings=await Review.findOne({
+    class_id:req.query.class_id,
+    given_by:classDetails.student_id,
+    teacher_id:classDetails.teacher_id
+  })
+
+    res.json(responseObj(true, { classDetails: classDetails, reminderResponse: reminderResponse,studentDetails:studentDetails,homeworkResponse:homeworkResponse,taskResponse:taskResponse,teacherDetails:teacherDetails,ratingsResponse:classRatingsResponse?classRatingsResponse.rating:0,teacherRatings:teacherRatings?teacherRatings.rating:0 }, null))
   }
   const getTrialClassDetails = async (req, res, next) => {
     let classDetails = {}

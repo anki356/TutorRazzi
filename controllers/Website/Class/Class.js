7d4e7df6837b5e9457c82a9654a98e270cb5b1d8
Ankita <anki356@gmil.com>
2024-02-15 18:51:57 +0530

    Save

--- controllers/Website/Class/Class.js
+++ controllers/Website/Class/Class.js
@@ -221,11 +221,16 @@ const getUpcomingClasses=async(req,res,next)=>{
 
   const getClassDetails = async (req, res, next) => {
     let classDetails = {}
-    classDetails = await Class.findOne({ _id: req.query.class_id }, { teacher_id:1,start_time: 1, end_time: 1, description: 1, grade: 1, subject_id: 1, notes: 1,  materials: 1,student_id:1  }).populate({
+    classDetails = await Class.findOne({ _id: req.query.class_id ,end_time:{
+      $lte:moment().format("YYYY-MM-DDTHH:mm:ss")
+    }}, { teacher_id:1,start_time: 1, end_time: 1, description: 1, grade: 1, subject_id: 1, notes: 1,  materials: 1,student_id:1  }).populate({
         path: 'teacher_id', select: {
             profile_image: 1, name: 1
         }
     })
+    if(!classDetails){
+      return res.json(responseObj(false,null,"Incorrect Class"))
+    }
  let teacherResponse=await Teacher.findOne({
     user_id:classDetails.teacher_id
  },{
@@ -258,7 +263,11 @@ let taskResponse=await Task.find({
 
 const getUpcomingClassDetails=async(req,res)=>{
   let classDetails = {}
-  classDetails = await Class.findOne({ _id: req.query.class_id }, { start_time: 1, end_time: 1, details: 1, grade: 1, subject_id: 1, teacher_id: 1, notes: 1,materials:1 }).populate({
+  classDetails = await Class.findOne({ _id: req.query.class_id,
+    start_time:{
+      $gte:moment().format("YYYY-MM-DDTHH:mm:ss")
+    }
+  }, { start_time: 1, end_time: 1, details: 1, grade: 1, subject_id: 1, teacher_id: 1, notes: 1,materials:1 }).populate({
     path: 'teacher_id', select: {
      name: 1,profile_image:1
     }
@@ -267,6 +276,9 @@ const getUpcomingClassDetails=async(req,res)=>{
       name: 1,mobile_number:1,profile_image:1
     }
   })
+  if(!classDetails){
+    return res.json(responseObj(false,null,"Incorrect Class"))
+  }
   let studentDetails=await Student.findOne({user_id:classDetails.student_id},{
     grade:1,
     curriculum:1,

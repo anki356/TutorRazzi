46f09f40e024f77bfac3456a3b0f9870d2b22d7b
Ankita <anki356@gmil.com>
2024-02-19 12:54:34 +0530

    Save

--- Routes/AcademicManager/Notification/Notification.js
+++ Routes/AcademicManager/Notification/Notification.js
@@ -1,7 +1,7 @@
 import express from 'express'
 const router=express.Router()
 import { authVerify } from '../../../controllers/AcademicManager/Auth/Auth.js'
-import {  clearNotification, getNotifications } from '../../../controllers/AcademicManager/Notification/Notification.js'
+import {   getNotificationDetails, getNotifications } from '../../../controllers/AcademicManager/Notification/Notification.js'
 router.get('/all-notifications',authVerify,getNotifications)
-router.patch('/clear-notifications',authVerify,clearNotification)
+router.get('/notification',authVerify,getNotificationDetails)
 export default router
--- controllers/AcademicManager/Notification/Notification.js
+++ controllers/AcademicManager/Notification/Notification.js
@@ -1,12 +1,35 @@
+import moment from "moment"
 import Notification from "../../../models/Notification.js"
 import { responseObj } from "../../../util/response.js"
 
 const getNotifications=async(req,res)=>{
-    const notifications=await Notification.find({user_id:req.user._id})
+  let query={
+    user_id:req.user._id,
+    createdAt:{
+      $gte:moment().subtract(1,"M").format("YYYY-MM-DDTHH:mm:ss"),
+      $lte:moment().format("YYYY-MM-DDTHH:mm:ss")
+    }
+  }
+  if(req.query.is_read){
+    query.is_read=req.query.is_read
+  }
+    const notifications=await Notification.find(query).sort({
+      createdAt:-1
+    })
     return res.json(responseObj(true,notifications,"Notifications"))
 }
-const clearNotification=async (req,res)=>{
-  await  Notification.deleteOne({user_id:req.user._id,_id:req.body.notification_id})
-    return res.json(responseObj(true,[],"Notifications Cleared"))
+const getNotificationDetails=async(req,res)=>{
+  const notificatioDetails=await Notification.findOneAndUpdate({
+    _id:req.query.id
+  },{
+    $set:{
+      is_read:true
+    }
+  })
+  return res.json(responseObj(true,notificatioDetails,"Notification Details"))
 }
-export {getNotifications,clearNotification}
+// const clearNotification=async (req,res)=>{
+//   await  Notification.deleteOne({user_id:req.user._id,_id:req.body.notification_id})
+//     return res.json(responseObj(true,[],"Notifications Cleared"))
+// }
+export {getNotifications,getNotificationDetails}

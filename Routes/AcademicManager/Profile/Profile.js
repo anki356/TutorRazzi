5d30e80cf2cfefd4f04951baf2aed7b0257a9a77
Ankita <anki356@gmil.com>
2024-02-19 15:21:45 +0530

    Save

--- Routes/AcademicManager/Profile/Profile.js
+++ Routes/AcademicManager/Profile/Profile.js
@@ -1,6 +1,6 @@
 import express from 'express'
-import { authVerify } from '../../../controllers/AcademicManager/Auth/Auth'
-import { editPhoto, editProfileDetails, getProfileDetails } from '../../../controllers/AcademicManager/Profile/Profile'
+import { authVerify } from '../../../controllers/AcademicManager/Auth/Auth.js'
+import { editPhoto, editProfileDetails, getProfileDetails } from '../../../controllers/AcademicManager/Profile/Profile.js'
 const router=express.Router()
 router.get("/profile",authVerify,getProfileDetails)
 router.patch("/profile",authVerify,editProfileDetails)

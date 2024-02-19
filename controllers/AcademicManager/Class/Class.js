65fdf31c6b3e125723f731043b61ad2ecd1a48ae
Ankita <anki356@gmil.com>
2024-02-19 11:37:31 +0530

    Save

--- controllers/AcademicManager/Class/Class.js
+++ controllers/AcademicManager/Class/Class.js
@@ -265,7 +265,7 @@ let options = {
         
     }],
     select: {
-      subject: 1, start_time: 1, name: 1, status: 1, class_type: 1, student_id: 1, teacher_id: 1,"end_time":1
+      subject: 1, start_time: 1, name: 1, status: 1, class_type: 1, student_id: 1, teacher_id: 1,"end_time":1,"is_rescheduled":1,"rescheduled_by":1
     }
 };
 

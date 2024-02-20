89289ed51148a5df7b2b06ace81c98a687598b3f
Ankita <anki356@gmil.com>
2024-02-20 15:30:45 +0530

    Save

--- Routes/AcademicManager/Notification/Notification.js
+++ Routes/AcademicManager/Notification/Notification.js
@@ -4,5 +4,5 @@ import { authVerify } from '../../../controllers/AcademicManager/Auth/Auth.js'
 import {   getNotificationDetails, getNotifications, markAllRead } from '../../../controllers/AcademicManager/Notification/Notification.js'
 router.get('/all-notifications',authVerify,getNotifications)
 router.get('/notification',authVerify,getNotificationDetails)
-router.get('/mark-all-read',authVerify,markAllRead)
+router.patch('/mark-all-read',authVerify,markAllRead)
 export default router

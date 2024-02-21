346fe6a40e0b38f306de0f5235d46e95acb5db74
Ankita <anki356@gmil.com>
2024-02-21 10:30:02 +0530

    Save

--- controllers/TeacherDashboard/Class/Class.js
+++ controllers/TeacherDashboard/Class/Class.js
@@ -418,7 +418,7 @@ const setReminder = async (req, res, next) => {
   }
   const getClassDetails=async(req,res)=>{
     let classDetails = {}
-  classDetails = await Class.findOne({ _id: req.query.class_id }).populate({
+  classDetails = await Class.findOne({ _id: req.query.class_id,end_time:{$lte:moment().format("YYYY-MM-DDTHH:mm:ss")}}).populate({
     path: 'teacher_id', select: {
      name: 1,profile_image:1
     }
@@ -427,6 +427,9 @@ const setReminder = async (req, res, next) => {
       name: 1
     }
   })
+  if(classDetails===null){
+  return res.json(responseObj(false,null,"No Class Found"))
+  }
   let studentDetails=await Student.findOne({user_id:classDetails.student_id},{
     grade:1,
     curriculum:1,
@@ -859,7 +862,9 @@ return res.json(responseObj(true,null,"Accepted Rescheduled Request"))
 
 const getUpcomingClassDetails=async(req,res)=>{
   let classDetails = {}
-  classDetails = await Class.findOne({ _id: req.query.class_id }, { start_time: 1, end_time: 1, details: 1, grade: 1, subject: 1, teacher_id: 1, notes: 1 }).populate({
+  classDetails = await Class.findOne({ _id: req.query.class_id,start_time:{
+    $gte:moment().format("YYYY-MM-DDTHH:mm:ss")
+  } }, { start_time: 1, end_time: 1, details: 1, grade: 1, subject: 1, teacher_id: 1, notes: 1 }).populate({
     path: 'teacher_id', select: {
      name: 1,profile_image:1
     }

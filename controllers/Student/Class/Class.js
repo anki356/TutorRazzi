593b2f935231978038547157722244d7be2ac07e
Ankita <anki356@gmil.com>
2024-02-23 14:13:46 +0530

    Save

--- controllers/Student/Class/Class.js
+++ controllers/Student/Class/Class.js
@@ -386,8 +386,8 @@ if(classResponse===null){
     return res.json(responseObj(false,null,"Invalid Class"))
 }
 console.log(classResponse.start_time)
-console.log(moment(),moment.utc(classResponse.start_time,"YYYY-MM-DDTHH:mm:ss").utc(), moment.utc(classResponse.end_time,"YYYY-MM-DDTHH:mm:ss").utc())
-    if (!(moment().isBetween(moment.utc(classResponse.start_time,"YYYY-MM-DDTHH:mm:ss"), moment.utc(classResponse.end_time,"YYYY-MM-DDTHH:mm:ss")))) {
+console.log(moment().utc(),moment.utc(classResponse.start_time,"YYYY-MM-DDTHH:mm:ss").utc(), moment.utc(classResponse.end_time,"YYYY-MM-DDTHH:mm:ss").utc())
+    if (!(moment().utc().isBetween(moment.utc(classResponse.start_time,"YYYY-MM-DDTHH:mm:ss"), moment.utc(classResponse.end_time,"YYYY-MM-DDTHH:mm:ss")))) {
         throw new Error('You cannot Join Class at this time')
     }
     console.log(classResponse.subject.name);

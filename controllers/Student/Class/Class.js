8b50850128034131b4064ba64f342d51835997b3
Ankita <anki356@gmil.com>
2024-02-23 13:02:49 +0530

    Save

--- controllers/Student/Class/Class.js
+++ controllers/Student/Class/Class.js
@@ -385,7 +385,7 @@ const joinClass = async (req, res, next) => {
 if(classResponse===null){
     return res.json(responseObj(false,null,"Invalid Class"))
 }
-console.log(moment().tz('Asia/Kolkata'),moment(classResponse.start_time).tz('Asia/Kolkata'), moment(classResponse.end_time).tz('Asia/Kolkata'))
+console.log(moment().tz('Asia/Kolkata'),moment(classResponse.start_time,"YYYY-MM-DDTHH:mm:ss").tz('Asia/Kolkata'), moment(classResponse.end_time,"YYYY-MM-DDTHH:mm:ss").tz('Asia/Kolkata'))
     if (!moment().tz('Asia/Kolkata').isBetween(moment(classResponse.start_time).tz('Asia/Kolkata'), moment(classResponse.end_time).tz('Asia/Kolkata'))) {
         throw new Error('You cannot Join Class at this time')
     }

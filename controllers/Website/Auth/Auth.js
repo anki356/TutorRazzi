573f03475aef41317dc7d2cce46bd2ab073f023b
Ankita <anki356@gmil.com>
2024-02-23 13:39:49 +0530

    Save

--- controllers/Website/Auth/Auth.js
+++ controllers/Website/Auth/Auth.js
@@ -36,7 +36,7 @@ const SignIn=async(req,res,next)=>{
     const { email, password } = req.body;
 
     const user = await User.findOne({ email });
-    if (!user &&!(user.role===req.body.key) ) {
+    if (!user &&(user.role!==req.body.key) ) {
         throw new Error("Invalid credentials or no user exist.");
     }
 if(!user.password){

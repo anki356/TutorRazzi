e582e345664e6a65e6dca495e5584f367a59f257
Ankita <anki356@gmil.com>
2024-03-02 19:13:26 +0530

    Svae

--- controllers/TeacherApp/Class/Class.js
+++ controllers/TeacherApp/Class/Class.js
@@ -90,7 +90,7 @@ const scheduleClass = async (req, res, next) => {
 
 const getClassDetails = async (req, res, next) => {
   let classDetails = {}
-  classDetails = await Class.findOne({ _id: req.query.class_id,$lte:moment().add(5,'h').add(30,'m').format("YYYY-MM-DDTHH:mm:ss") }, { start_time: 1, end_time: 1, description: 1, grade: 1, subject: 1, teacher_id: 1, notes: 1,  materials: 1, recordings: 1,class_type:1 }).populate({
+  classDetails = await Class.findOne({ _id: req.query.class_id,end_time:{$lte:moment().add(5,'h').add(30,'m').format("YYYY-MM-DDTHH:mm:ss")} }, { start_time: 1, end_time: 1, description: 1, grade: 1, subject: 1, teacher_id: 1, notes: 1,  materials: 1, recordings: 1,class_type:1 }).populate({
     path: 'teacher_id', select: {
      name: 1,profile_image:1
     }

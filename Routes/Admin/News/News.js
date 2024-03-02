0d7085b3ee8a2e3d862edcfc14c2cecc1605a2dd
Ankita <anki356@gmil.com>
2024-03-02 12:59:05 +0530

    Save

--- Routes/Admin/News/News.js
+++ Routes/Admin/News/News.js
@@ -1,14 +1,15 @@
 import express from 'express'
 import { authVerify } from "../../../controllers/Admin/Auth/Auth.js";
-import { addNews } from '../../../controllers/Admin/News/News.js'
+import { addNews, editNews } from '../../../controllers/Admin/News/News.js'
 import { body } from 'express-validator';
 import validationError from '../../../middleware/validationError.js';
 const router=express.Router()
 const newsValidation=[
    body('title').notEmpty().withMessage("Title is required") ,
-   body('sub_title').notEmpty().withMessage("Sub Title is required"),
    body('description').notEmpty().withMessage("Description is required"),
    body('category').notEmpty().withMessage("Category is required")
 ]
+
 router.post("/news",authVerify,newsValidation,validationError,addNews)
+router.patch("/news/:_id",authVerify,editNews)
 export default router
--- controllers/Admin/News/News.js
+++ controllers/Admin/News/News.js
@@ -1,15 +1,37 @@
 import News from "../../../models/News.js"
 import { responseObj } from "../../../util/response.js"
+import upload from "../../../util/upload.js"
 
 const addNews=async(req,res)=>{
+    if(req.files.cover_photo){
+        req.body.cover_photo=await upload(req.files.cover_photo)
+          
     const newsResponse=await News.create({
         title: req.body.title,
         description:req.body.description,
         category:req.body.category,
-        cover_photo:req.files[0].filename
+        cover_photo:req.body.cover_photo
     })
     res.json(responseObj(true,newsResponse,"News Added"))
 }
+else{
+    res.json(responseObj(false,null,"No cover Photo provided"))
+}
 
+    res.json(responseObj(true,newsResponse,"News Added"))
+}
+const editNews=async(req,res)=>{
+  if(req.files.cover_photo){
+req.body.cover_photo=await upload(req.files.cover_photo)
+  }
+  
+    await News.updateOne({
+        _id:req.params._id
+    },{
+        $set:{
+            ...req.body
+        }
+    })
+}
 
-export {addNews}
+export {addNews,editNews}

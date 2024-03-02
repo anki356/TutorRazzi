c18415b7848218931b2bac5fe027128cd031d53a
Ankita <anki356@gmil.com>
2024-03-02 11:36:38 +0530

    Save

--- Routes/Admin/Class/Class.js
+++ Routes/Admin/Class/Class.js
@@ -1,6 +1,6 @@
 import express from 'express'
-import { authVerify } from '../../../controllers/Student/Auth/Auth'
-import { getPastClasses, getRescheduledClasses, getTrialClasses, getUpcomingClasses } from '../../../controllers/Admin/Class/Class'
+import { authVerify } from '../../../controllers/Student/Auth/Auth.js'
+import { getPastClasses, getRescheduledClasses, getTrialClasses, getUpcomingClasses } from '../../../controllers/Admin/Class/Class.js'
 
 const router=express.Router()
 router.get("/get-upcoming-classes",authVerify,getUpcomingClasses)

d142ac317b32db34daa232ec9f3c02b4f5de9667
Ankita <anki356@gmil.com>
2024-03-11 14:00:27 +0530

    Save

--- Routes/Parent/Auth/Auth.js
+++ Routes/Parent/Auth/Auth.js
@@ -8,11 +8,13 @@ const loginValidationChain = [
 body('password').notEmpty().withMessage('Password is required field.')
     .isLength({ min: 8 }).optional()
     .withMessage('Password should have atleast 8 characters.')];
-
+    const passwordValidationChain = [
+        // body('email').notEmpty().isEmail().trim(),
+    body('password').notEmpty().withMessage('Password is required field.')]
 
 router.post("/SignUp",loginValidationChain,validationError,SignUp)
 router.post("/SignIn",loginValidationChain,validationError,SignIn)
-router.patch("/change-Password",loginValidationChain,validationError,changePassword)
+router.patch("/change-Password",passwordValidationChain,validationError,changePassword)
 router.post("/verify-Email",loginValidationChain,validationError,verifyEmail)
 router.post("/verify-OTP",verifyOTP)
 export default router

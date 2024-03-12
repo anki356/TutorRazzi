cb69167c44198e48dc0d9fc6ee5cb25c98cdeb3d
Ankita <anki356@gmil.com>
2024-03-12 14:21:55 +0530

    Save

--- controllers/TeacherApp/Chat/Chat.js
+++ controllers/TeacherApp/Chat/Chat.js
@@ -23,8 +23,8 @@ console.log(req.user._id)
     if (orConditions.length > 0) {
         query.$or = orConditions;
     }
-
-    const students = Class.aggregate([
+console.log(query)
+    const students = await Class.aggregate([
         {
                 $match: query
             },
@@ -86,6 +86,12 @@ console.log(req.user._id)
             },
         },
         {
+            $skip: (Number(page) - 1) * Number(limit)
+        },
+        {
+            $limit:Number(limit)
+        },
+        {
             $group: {
                 _id: "$user._id",
                 uniqueEntries: { $addToSet: "$$ROOT" }
@@ -94,90 +100,89 @@ console.log(req.user._id)
         
      
     ]);
-    // const studentsTotal = await Class.aggregate([
-    //     {
-    //             $match: query
-    //         },
-    //     {
-    //         $lookup: {
-    //             from: 'users',
-    //             localField: 'student_id',
-    //             foreignField: '_id',
-    //             as: 'user',
-    //             pipeline: [
-    //                 {
-    //                     $addFields: { profile: { $concat: [process.env.APP_URL, '$profile_image'] } }
-    //                 },
-    //                 {
-    //                     $project: { name: 1, _id: 1, role: 1,profile:1 }
-    //                 },
-    //             ],
-    //         },
-    //     },
+    const studentsTotal = await Class.aggregate([
+        {
+                $match: query
+            },
+        {
+            $lookup: {
+                from: 'users',
+                localField: 'student_id',
+                foreignField: '_id',
+                as: 'user',
+                pipeline: [
+                    {
+                        $addFields: { profile: { $concat: [process.env.APP_URL, '$profile_image'] } }
+                    },
+                    {
+                        $project: { name: 1, _id: 1, role: 1,profile:1 }
+                    },
+                ],
+            },
+        },
        
-    //     {
-    //         $addFields: {
-    //             nonEmptyFields: {
-    //                 $filter: {
-    //                     input: [
-    //                         { $arrayElemAt: ["$students", 0] },
-    //                     ],
-    //                     as: 'field',
-    //                     cond: { $ne: ['$$field', []] },
-    //                 },
-    //             },
-    //         },
-    //     },
-    //     {
-    //         $addFields: {
-    //             nonEmptyFields: {
-    //                 $cond: {
-    //                     if: { $eq: [{ $size: '$nonEmptyFields' }, 0] },
-    //                     then: [{}], // Use an empty array if there are no non-empty fields
-    //                     else: '$nonEmptyFields',
-    //                 },
-    //             },
-    //         },
-    //     },
-    //     {
-    //         $unwind: '$user'
-    //     },
+        {
+            $addFields: {
+                nonEmptyFields: {
+                    $filter: {
+                        input: [
+                            { $arrayElemAt: ["$students", 0] },
+                        ],
+                        as: 'field',
+                        cond: { $ne: ['$$field', []] },
+                    },
+                },
+            },
+        },
+        {
+            $addFields: {
+                nonEmptyFields: {
+                    $cond: {
+                        if: { $eq: [{ $size: '$nonEmptyFields' }, 0] },
+                        then: [{}], // Use an empty array if there are no non-empty fields
+                        else: '$nonEmptyFields',
+                    },
+                },
+            },
+        },
+        {
+            $unwind: '$user'
+        },
         
 
-    //     {
-    //         $replaceRoot: {
-    //             newRoot: {
-    //                 // student: {$mergeObjects: ["$user", { $arrayElemAt: ['$nonEmptyFields', 0] }]},
-    //                 // counselor: '$counselor',
-    //                 _id: { $arrayElemAt: ['$nonEmptyFields._id', 0] },
-    //                 student: { $mergeObjects: [{ $arrayElemAt: ['$nonEmptyFields', 0] }, "$user"] },
-    //                 teacher: req.user._id,
-    //             },
-    //         },
-    //     },
-    //     {
-    //         $group: {
-    //             _id: "$user._id",
-    //             uniqueEntries: { $addToSet: "$$ROOT" }
-    //         }
-    //     },
+        {
+            $replaceRoot: {
+                newRoot: {
+                    // student: {$mergeObjects: ["$user", { $arrayElemAt: ['$nonEmptyFields', 0] }]},
+                    // counselor: '$counselor',
+                    _id: { $arrayElemAt: ['$nonEmptyFields._id', 0] },
+                    student: { $mergeObjects: [{ $arrayElemAt: ['$nonEmptyFields', 0] }, "$user"] },
+                    teacher: req.user._id,
+                },
+            },
+        },
+        {
+            $group: {
+                _id: "$user._id",
+                uniqueEntries: { $addToSet: "$$ROOT" }
+            }
+        },
        
      
-    // ]);
+    ]);
     
 
-    // if (!students.length>0) {
-    //     return res.json(responseObj(true,[],"No users"));
-    // }
+  
     
-Class.aggregatePaginate(students,options,(err,result)=>{
-    if(result){
-        if(result.totalDocs===0){
-            return res.json(responseObj(false,null,"No users"));
-        }
-    }
-    return res.json(responseObj(true,result,"All users"));
-})
-    
+   let totalDocs=studentsTotal[0].uniqueEntries.length
+   if (totalDocs===0) {
+    return res.json(responseObj(false,null,"No users"));
+}
+   let totalPages=Math.ceil(totalDocs/Number(limit))
+   let hasPrevPage=page>1
+   let hasNextPage=page<totalPages
+   let prevPage=hasPrevPage?Number(page)-1:null
+   let nextPage=hasNextPage?Number(page)+1:null
+    return res.json(responseObj(true,{docs:students[0].uniqueEntries,totalDocs:totalDocs,limit:limit,page:page,pagingCounter:page,totalPages:totalPages,hasNextPage:hasNextPage,hasPrevPage:hasPrevPage,prevPage:prevPage,nextPage:nextPage},"All users"));
 }
  export {getAll}  

93f853438a3a398695455995b34faa5a35817e25
Ankita <anki356@gmil.com>
2024-03-13 14:30:13 +0530

    Save

--- util/removeTemp.js
+++ util/removeTemp.js
@@ -1,21 +1,21 @@
 import fs from 'fs'
 import path from 'path'
-import { Storage } from '@google-cloud/storage'
+// import { Storage } from '@google-cloud/storage'
 async function removeTemp(filePath) {
     try {
-        const projectId = process.env.PROJECT_ID
-        const bucketName = process.env.BUCKET_NAME
-        const serviceKey = path.join(path.resolve(),  'storageservice.json')
+        // const projectId = process.env.PROJECT_ID
+        // const bucketName = process.env.BUCKET_NAME
+        // const serviceKey = path.join(path.resolve(),  'storageservice.json')
+        
+        // const storage = new Storage({ projectId, keyFilename: serviceKey });
+        // const bucket = storage.bucket(bucketName);
 
-        const storage = new Storage({ projectId, keyFilename: serviceKey });
-        const bucket = storage.bucket(bucketName);
+        // const file = bucket.file(filePath);
 
-        const file = bucket.file(filePath);
-
-        const exists = await file.exists();
-       
-        if (exists) {
-            await file.delete();
+        // const exists = await file.exists();
+     
+        if (fs.existsSync(filePath)) {
+             fs.rmSync(filePath)
         }
     } catch (error) {
         console.error(`Error deleting file '${filePath}': ${error.message}`);

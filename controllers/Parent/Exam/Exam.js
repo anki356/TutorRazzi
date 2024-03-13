61cfe77c36e8f634367c38246ff04a1aab3ee8e9
Ankita <anki356@gmil.com>
2024-03-13 12:31:52 +0530

    Same

--- controllers/Parent/Exam/Exam.js
+++ controllers/Parent/Exam/Exam.js
@@ -3,7 +3,7 @@ import Student from "../../../models/Student.js"
 import { responseObj } from "../../../util/response.js"
 
 const addExams=async(req,res,next)=>{
-    const studentExamInsertResponse=await Exam.create({student_id:req.body.student_id ,subject:{name:req.body.subject},
+    const studentExamInsertResponse=await Exam.create({student_id:req.user._id ,subject:{name:req.body.subject},
         start_time:req.body.start_time,
         end_time:req.body.end_time,
         syllabus:req.body.syllabus,

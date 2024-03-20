d17f488e31e9e373d18cfb1864d5c26b8768c032
Ankita <anki356@gmil.com>
2024-03-20 14:35:20 +0530

    Save

--- controllers/TeacherApp/Class/Class.js
+++ controllers/TeacherApp/Class/Class.js
@@ -951,7 +951,7 @@ const viewRec=async(req,res)=>{
   }
   const getTrialClassDetails = async (req, res, next) => {
     let classDetails = {}
-    classDetails = await Class.findOne({ _id: req.query.class_id,class_type:"Trial" }, { start_time: 1, end_time: 1, details: 1, grade: 1, subject: 1, teacher_id: 1, notes: 1,  materials: 1, recordings: 1,response:1,reason_disliking:1,curriculum:1,status:1 }).populate({
+    classDetails = await Class.findOne({ _id: req.query.class_id,class_type:"Trial" }, { start_time: 1, end_time: 1, details: 1, grade: 1, subject: 1, teacher_id: 1, notes: 1,  materials: 1, recordings: 1,response:1,reason_disliking:1,curriculum:1,status:1 ,is_rescheduled:1}).populate({
       path: 'teacher_id', select: {
        name: 1,profile_image:1
       }
